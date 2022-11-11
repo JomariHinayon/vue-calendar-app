8877839e099b27e549ac809e4ca68364a006e411
JomariHinayon <hinayonjomari@gmail.com>
2022-11-11 23:49:58 +0800

    Calendar Finish

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="flex flex-row w-screen justify-center items-center">
-        <div class="flex flex-col w-1/2 mt-5 text-center">
+    <div class="flex flex-row w-screen justify-center items-center ">
+        <div class="flex flex-col w-[45%] mt-5 text-center ">
             <!-- Months -->
+            {{ currentDate() }}
             <div class="pt-5 pb-2 flex flex-row justify-center">
-                <button @click="minusMonth()">
+                <button @click="decreaseMonth()">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
                     </svg>
                 </button>
-                <p class="text-xl font-semibold px-5">{{ currentMonth }}</p>
-                <button @click="addMonth()">
+                <p class="text-xl font-semibold px-5">{{ currentMonthName }}</p>
+                <button @click="increaseMonth()">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                     </svg>
@@ -32,9 +33,16 @@
             <!-- Days -->
             <div class="flex flex-row flex-wrap mt-3 ">
                 <p
-                 v-for="day in 31"
+                 v-for="day in weekDayStart()"
                  :key="day"
                  class="w-[5.5rem] mt-2"
+                >
+                </p>
+                <p
+                 v-for="day in daysInMonth()"
+                 :key="day"
+                 class="w-[5.5rem] mt-2"
+                 :class=" currentDate(day) ? 'bg-red-500 rounded-full text-white' : ''"
                 >
                  {{ day }}
                 </p>
@@ -49,32 +57,42 @@
         name: 'Calendar',
         data() {
             return {
-                weekdays: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
-                monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-                monthIndex: new Date().getMonth(),
-                currentMonth: new Date().toLocaleString('default', {month: "long"}),
+                weekdays: ['Sun','Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
+                months : ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+                currentMonth: new Date().getMonth(),
+                currentMonthName: new Date().toLocaleString('default', {month: "long"}),
                 currentYear: new Date().getFullYear(),
-
             }
         },
         methods: {
-            minusMonth() {
-                if(this.monthIndex > 0) {
-                    this.currentMonth = this.monthNames[this.monthIndex -= 1];
+            daysInMonth() {
+                return new Date(this.currentYear, this.currentMonth + 1, 0).getDate();
+            },
+            weekDayStart(){
+                return new Date(this.currentYear, this.currentMonth + 1, 0).getDay(); 
+            },
+            decreaseMonth() {
+                if(this.currentMonth > 0) {
+                    this.currentMonthName = this.months[this.currentMonth -= 1];
                 }else {
-                    this.monthIndex = this.monthNames.length - 1;
-                    this.currentMonth = this.monthNames[this.monthIndex];
-                    this.currentYear = Number(this.currentYear) - 1;
+                    this.currentMonth = this.months.length - 1;
+                    this.currentMonthName = this.months[this.currentMonth];
+                    this.currentYear -= 1;
                 }
             },
-            addMonth() {
-                if(this.monthIndex < this.monthNames.length - 1) {
-                    this.currentMonth = this.monthNames[this.monthIndex += 1];   
+            increaseMonth() {
+                if(this.currentMonth < this.months.length - 1) {
+                    this.currentMonthName = this.months[this.currentMonth += 1];
                 }else {
-                    this.monthIndex = 0;
-                    this.currentMonth = this.monthNames[this.monthIndex];
-                    this.currentYear = Number(this.currentYear) + 1;
+                    this.currentMonth = 0;
+                    this.currentMonthName = this.months[this.currentMonth];
+                    this.currentYear += 1;
                 }
+            },
+            currentDate(day) {
+                const currentDate = new Date().toDateString();
+                const viewDate = new Date(this.currentYear, this.currentMonth, day).toDateString();
+                return currentDate == viewDate;
             }
         }
     }
